f3f050c34623bb1e155121be97a66254e2da84fa
Eduardo Leonne <chamascadentes@gmail.com>
2026-06-04 20:18:08 -0400

    conditional for gaming vs desktop mode

--- steamdeck-client/launch.sh
+++ steamdeck-client/launch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-export SDL_VIDEODRIVER=wayland
-export SDL_AUDIODRIVER=pipewire
+
+# In Game Mode, Gamescope provides Wayland. In Desktop Mode, use x11.
+if [ "$SteamGamepadUI" = "1" ]; then
+    export SDL_VIDEODRIVER=wayland
+    export SDL_AUDIODRIVER=pipewire
+else
+    export SDL_VIDEODRIVER=x11
+fi
+
 export PYTHONPATH="/home/deck/awake-on-deck/steamdeck-client/lib:$PYTHONPATH"
 cd /home/deck/awake-on-deck/steamdeck-client
 exec /usr/bin/python3 main.py "$@"
